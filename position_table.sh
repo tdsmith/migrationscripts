c5aea917e1257b35d72bae3df2afef9b2e137493
Tim D. Smith <git@tim-smith.us>
2014-09-10 13:11:35 -0700

    make position_table generate csv

--- position_table.sh
+++ position_table.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jq -c '[.POSITIONS[] | .LABEL]|. as $labels | range(0; length) as $idx | [$idx+1, $labels[$idx]]'
+jq -r '[.POSITIONS[] | .LABEL]|. as $labels | range(0; length) as $idx | @text "\($idx+1), \($labels[$idx])"'
