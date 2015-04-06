f3cf035130362ff28e5344b8481e99075d7f5d24
Tim D. Smith <git@tim-smith.us>
2015-04-06 14:13:11 -0700

    extrude: tiffs encode number of locations already

--- extrude.py
+++ extrude.py
@@ -5,21 +5,19 @@
 Reduces bit depth from 12 bits to 8 bits."""
 
 from __future__ import print_function
-import os.path
+import os
 import tifffile as tf
 import numpy as np
-from distutils.dir_util import mkpath
 
-def extrude(filename, outpath, n_positions, handler=print):
-    tiff = tf.TiffFile(filename)
-    assert len(tiff) % n_positions == 0
-    # n_timepoints = len(tiff) / n_positions
-    mkpath(outpath)
-    for p in xrange(n_positions):
+def extrude(filename, outpath, handler=print):
+    tiff = tf.TiffFile(filename, multifile=True, multifile_close=False)
+    serieses = tiff.series
+    os.makedirs(outpath)
+    for p, series in enumerate(serieses):
         handler(p)
         outfn = os.path.join(outpath, "{}.tif".format(p))
         with tf.TiffWriter(outfn) as tw:
-            array = tf.stack_pages(tiff[p::n_positions])
+            array = tf.stack_pages(series['pages'])
             array >>= 4
             array = array.astype(np.uint8)
             tw.save(array)
@@ -29,6 +27,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("input_stack")
     parser.add_argument("output_path")
-    parser.add_argument("n_positions", type=int)
     args = parser.parse_args()
-    extrude(args.input_stack, args.output_path, args.n_positions)
+    extrude(args.input_stack, args.output_path)
