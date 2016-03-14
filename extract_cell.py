dce0fd0c6f2f4ed05f098a8c784729893aab46bb
Tim D. Smith <git@tim-smith.us>
2016-03-14 15:25:34 -0700

    Operate on multi-page TIFFs

--- extract_cell.py
+++ extract_cell.py
@@ -4,9 +4,10 @@ import numpy as np
 import pandas as pd
 import tifffile as tf
 
+
 def extract_window(image, x, y, h, w):
     hw, hh = w//2, h//2
-    sample = image[max(0, y-hh):y+hh, max(0,x-hw):x+hw]
+    sample = image[max(0, y-hh):y+hh, max(0, x-hw):x+hw]
     top_pad = abs(y-hh) if y-hh < 0 else 0
     left_pad = abs(x-hw) if x-hw < 0 else 0
     sh, sw = sample.shape
@@ -15,3 +16,9 @@ def extract_window(image, x, y, h, w):
     return output
 
 
+def stack_extract_window(stack, xlist, ylist, h, w):
+    n = stack.shape[0]
+    out = np.zeros((n, h, w), dtype=stack.dtype)
+    for i, (im, x, y) in enumerate(zip(stack, xlist, ylist)):
+        out[i] = extract_window(im, x, y, h, w)
+    return out
--- test_extract_cell.py
+++ test_extract_cell.py
@@ -6,9 +6,11 @@ from extract_cell import *
 
 WRITE_OUTPUT = False
 
+
 class TestExtractWindow(unittest.TestCase):
     def setUp(self):
         self.single_page = tf.imread("test_fixtures/single_page.tif")
+        self.multi_page = tf.imread("test_fixtures/multi_page.tif")
 
     def test_extract_window(self):
         for i, (x, y) in enumerate(((0, 0), (100, 100), (250, 250))):
@@ -17,6 +19,18 @@ class TestExtractWindow(unittest.TestCase):
             self.assertEqual(window.shape, (200, 200))
             if WRITE_OUTPUT:
                 tf.imsave("test_extract_window_%d.tif" % i, window)
+
+    def test_stack_extract_window(self):
+        xlist = np.array([164, 165, 157, 142])
+        ylist = np.array([208, 208, 194, 172])
+        extracted = stack_extract_window(self.multi_page, xlist, ylist, 200, 200)
+        self.assertIsInstance(extracted, np.ndarray)
+        self.assertEqual(extracted.shape, (4, 200, 200))
+        if WRITE_OUTPUT:
+            tf.imsave("test_stack_extract_window.tif",
+                      extracted,
+                      photometric="minisblack")
+
 
 if __name__ == "__main__":
     import sys
